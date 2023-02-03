f74daf84f1559078532682cedc5a56ce5b4c18be
Hubson777 <gloshubert@gmail.com>
2023-02-03 06:30:55 +0100

    zadanie dzień 4

--- ChallengeApp/Program.cs
+++ ChallengeApp/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello, World!");
+﻿string name = "Ewa";
+bool female = true;
+int age = 33;
+
+if(age < 30 && female == true)
+{
+    Console.WriteLine("kobieta, poniżej lat 30");
+}
+else if (name == "Ewa" && age == 33) 
+{
+    Console.WriteLine("Ewa, lat 33");
+} 
+else if (age < 18 && female == false) { Console.WriteLine("niepełnoletni mężczyzna");
+}
